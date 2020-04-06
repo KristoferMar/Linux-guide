a1c90433bf25df06045f7e80a8b4d3b9af566d30
Kristofer Mar Einarsson <kristofer@brostu.dk>
2020-04-06 18:20:00 +0200

    Update Commands.sh

--- Directories/Commands.sh
+++ Directories/Commands.sh
@@ -1,2 +1,11 @@
+# Create a new directory 
+mkdir
+
+# Edit name of a directory
+mv <source_directory> <target_directory>
+
+# Delete directory 
+rmdir <source_directory>
+
 # Find a specific directory on linux
 find / -type d -name "dir-name-here"
